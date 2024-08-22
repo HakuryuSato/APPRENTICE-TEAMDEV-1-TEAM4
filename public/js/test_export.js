b2763401ad90e1d3e812280184773a7abea63033
HakuryuSato <r700540@gmail.com>
2024-08-22 17:21:09 +0900

    ファイル名をjsからmjsへ変更(nodeによるデバッグのため

--- public/js/test_export.js
+++
@@ -1,4 +0,0 @@
-export const greeting = "Hello, world!";
-export function greet(name) {
-    return `Hello, ${name}!`;
-}
--- public/js/test_import.js
+++
@@ -1,4 +0,0 @@
-import { greeting, greet } from './test_export.js';
-
-console.log(greeting); // "Hello, world!"
-console.log(greet("Alice")); // "Hello, Alice!"
