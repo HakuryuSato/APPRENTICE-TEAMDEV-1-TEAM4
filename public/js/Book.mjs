0e3444ba06655cd3b7a944f3a7f7da289459c344
HakuryuSato <r700540@gmail.com>
2024-08-27 18:49:48 +0900

    文字を中央ぞろえにするよう変更

--- public/js/Book.mjs
+++ public/js/Book.mjs
@@ -3,8 +3,8 @@ export class Book {
     // コンストラクタ引数として、
     // fetch_booksで受け取ったbookデータをプロパティに設定
     constructor(
-        sessionId, // 
-        coverColor, // 'FF000000'
+        sessionId, //
+        coverColor, // 'red'
         coverTextColor, //
         categoryName, // 'PHP'
         bookThickness, // 1 ~ 4
@@ -18,33 +18,37 @@ export class Book {
 
     draw(ctx, x, y) {
         const bookImage = new Image();
-        const bookHeight = 237
+        const bookHeight = 237;
 
         // 厚みに応じて画像選択
-        bookImage.src = `./images/book_red_${this.bookThickness}.png`;
-        
+        bookImage.src = `./images/book_${this.coverColor}_${this.bookThickness}.png`;
+
         // // テスト用
         // bookImage.src = './images/book.png'
-        
 
         bookImage.onload = () => { // ブラウザにオブジェクトを読み込んだ時に発火
 
-            // 背景色を設定して本の四角形を描画 // 色をHEXカラーコードで変更するならば使用する。
-            // ctx.fillStyle = this.coverColor;
-            // ctx.fillRect(x, y, 20 + this.bookThickness, bookHeight);
-
             // 本の画像を描画 引数(画像, 描画するx座標, 描画y座標, 横幅, 高さ)
-            // 注意：本の厚みに応じて値変更する必要がある。
-            ctx.drawImage(bookImage, x, y, this.bookThickness * 20 , bookHeight);
+            ctx.drawImage(bookImage, x, y, this.bookThickness * 20, bookHeight);
 
             // テキストを縦書きで描画 *本の厚みで余裕あれば描画？
             ctx.fillStyle = this.coverTextColor;
 
+            // フォントと配置位置
             ctx.font = "18px sans-serif";
             ctx.textBaseline = "top";
 
+            // テキストの描画開始位置を計算
+            const textX = x + (this.bookThickness * 20) / 2; // 本の厚みの中心
+
+            // テキストを描画
             for (let i = 0; i < this.categoryName.length; i++) {
-                ctx.fillText(this.categoryName[i], x + 5, y + 5 + i * 20);
+                const textWidth = ctx.measureText(this.categoryName[i]).width; // 各文字の幅を計算
+                ctx.fillText(
+                    this.categoryName[i],
+                    textX - textWidth / 2,
+                    y + 5 + i * 20,
+                ); 
             }
 
 
