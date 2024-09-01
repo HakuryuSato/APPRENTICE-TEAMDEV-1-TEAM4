b95a24fe2218d3ec6090be05923e6fa7e82149ba
HakuryuSato <r700540@gmail.com>
2024-09-01 19:19:57 +0900

    モーダル管理用コード追加

--- public/js/main.mjs
+++ public/js/main.mjs
@@ -2,12 +2,74 @@
 import { BookshelfRenderer } from "./BookshelfRenderer.mjs";
 import { Shelf } from "./Shelf.mjs";
 import { Book } from "./Book.mjs";
+import { Modal } from "./Modal.mjs";
 
 // DB通信用関数
 import { fetchBooks } from "./fetch_books.mjs";
 
-// 佐藤：一時的に本データの取得と描画を直接記述していますが、
-//      モーダルの管理などが加わった際に分割する必要があります。
+// イベントリスナー群  ---------------------------------------------------------------------------------------------------
+// モーダル関連
+document.addEventListener("DOMContentLoaded", () => { // DOMロード完了後
+    
+
+    // モーダル用インスタンス
+    const ijinModal = new Modal("ijin-modal"); // 偉人モーダル
+    const registerModal = new Modal("register-modal"); // 登録モーダル
+    const registerConfirmModal = new Modal("register-confirm-modal"); // 確認用モーダル
+
+
+    // ボタン群
+    // 勉強開始ボタン
+    const startStudyButton = document.getElementById(
+        "start-study-button",
+    );
+    // 今日の勉強を終えるボタン
+    const endTodaysStudyButton = document.getElementById("end-todays-study");
+
+    // 偉人閉じるボタン
+    const ijinModalCloseButton = document.getElementById("ijin-modal-close");
+
+    // 登録確認ボタン
+    const registerConfirmButton = document.getElementById(
+        "register-confirm-button",
+    );
+
+
+    // クリックイベントリスナー群 
+    // 偉人モーダル表示
+    registerConfirmButton.addEventListener("click", () => {
+        ijinModal.open();
+    });
+
+    // 偉人モーダル閉じる
+    ijinModalCloseButton.addEventListener("click", () => {
+        ijinModal.close();
+    });
+
+    // 登録モーダル表示
+    startStudyButton.addEventListener("click", () => {
+        registerModal.open();
+    });
+
+    // 登録モーダル閉じる
+    registerConfirmButton.addEventListener("click", () => {
+        registerModal.close();
+    });
+
+    // 登録確認モーダル表示
+    endTodaysStudyButton.addEventListener("click", () => {
+        registerConfirmModal.open();
+    });
+
+    // 登録確認モーダル非表示
+    registerConfirmButton.addEventListener("click", () => {
+        registerConfirmModal.close();
+    });
+});
+
+//  ---------------------------------------------------------------------------------------------------
+
+
 
 // DBから本のデータ取得
 const inputData = await fetchBooks();
