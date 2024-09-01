c3fe82f28ec8b54c654ccfcfbdc9a1a7e5172d5e
HakuryuSato <r700540@gmail.com>
2024-09-01 18:13:33 +0900

    偉人モーダル用HTMLを追加(最小構成のみ)

---
+++ src/views/ijin_modal.php
@@ -0,0 +1,7 @@
+<div id="ijin_modal" class="modal" types="">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <a>偉人のありがたい言葉</a>
+        <button id='ijin_modal_close'>ありがとう</button>
+    </div>
+</div>
