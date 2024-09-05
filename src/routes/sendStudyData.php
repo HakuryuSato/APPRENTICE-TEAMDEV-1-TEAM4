a81c2b65a7fa0cc729a46d73eae224baec13f832
HakuryuSato <r700540@gmail.com>
2024-09-05 21:57:28 +0900

    エンドポイント追加

--- src/routes/sendStudyData.php
+++ src/routes/sendStudyData.php
@@ -5,13 +5,31 @@ require_once __DIR__ . '/../controllers/BookController.php';
 use Controllers\BookController;
 
 try {
-    // POSTリクエストからデータを受け取る
-    $postData = file_get_contents('php://input');
-    $bookData = json_decode($postData, true);
+    // 本番用
+    // $postData = file_get_contents('php://input');
+    // $studyData = json_decode($postData, true);
+
+
+    // テスト用
+    $studyData = [
+        [
+            'category_name' => 'PHP',
+            'session_duration_minutes' => 30,
+        ],
+        [
+            'category_name' => 'Laravel',
+            'session_duration_minutes' => 138,
+        ],
+    ];
+
+
+
+
+
 
     // BookControllerのインスタンスを作成し、メソッドを呼び出す
     $controller = new BookController();
-    $result = $controller->saveBookData($bookData);
+    $result = $controller->sendStudyData($studyData);
 
     // 処理結果をJSON形式で返す
     header('Content-Type: application/json');
