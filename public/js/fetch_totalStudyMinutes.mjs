9058860cbbca461253fad3cb08dc5234c15c3657
HakuryuSato <r700540@gmail.com>
2024-09-07 19:45:30 +0900

    合計学習時間取得用fetch関数追加

---
+++ public/js/fetch_totalStudyMinutes.mjs
@@ -0,0 +1,18 @@
+// バックエンドのgetBooks.phpを呼び出す関数
+export async function fetchTotalStudyMinutes() {
+    try {
+        const response = await fetch("http://localhost:80/src/routes/getTotalStudyMinutes.php");
+        if (!response.ok) { // リクエスト失敗ならエラー処理
+            throw new Error("Network response was not ok");
+        }
+        
+        // 成功ならデータを返す
+        const data = await response.json();
+
+        // console.log(data);
+        return data;
+    } catch (error) {
+        console.error("Fetch error:", error);
+        return null; // エラー時に null を返す
+    }
+}
