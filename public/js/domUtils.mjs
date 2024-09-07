2b345f1ad30d0ccf6b78c058aae17f342e8ad732
hiderits8 <wataru.ts8.hideri@gmail.com>
2024-09-07 13:35:19 +0900

    change: CSSの調整。一部jsの書き換え。

--- public/js/domUtils.mjs
+++ public/js/domUtils.mjs
@@ -23,16 +23,17 @@ function createRow(finishCategory)
     const newRow = document.createElement('tr');
 
     // カテゴリ、時間、削除ボタンのtd要素を追加
-    newRow.appendChild(createTd('history_category', finishCategory.category_name));
-    newRow.appendChild(createTd('sum_time', formatTime(finishCategory.session_duration_minutes)));
+    newRow.appendChild(createTd('history_category', 'history_category', finishCategory.category_name));
+    newRow.appendChild(createTd('timer', 'sum_time', formatTime(finishCategory.session_duration_minutes)));
     newRow.appendChild(createDeleteButtonTd());
 
     return newRow;
 };
 
-function createTd(id, textContent)
+function createTd(className, id, textContent)
 {
     const td = document.createElement('td');
+    td.setAttribute('class', className);
     td.setAttribute('id', id);
     td.innerText = textContent;
     return td;
@@ -41,13 +42,14 @@ function createTd(id, textContent)
 function createDeleteButtonTd()
 {
   const td = document.createElement('td');
+  td.id = 'delete';
   const btnDelete = document.createElement('button');
 
   btnDelete.className = 'recent';
   btnDelete.id = 'btn_delete';
   btnDelete.name = 'btn_delete';
   btnDelete.type = 'button';
-  btnDelete.textContent = '削除';
+  btnDelete.innerHTML = '';
   btnDelete.addEventListener('click', (e) => deleteList(e));
   td.appendChild(btnDelete);
   return td;
