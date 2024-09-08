fb04f260e3b7a50a809bfc9567ef01cd8bac92ec
marufoi22 <m.y.882soccer@gmail.com>
2024-09-08 15:05:58 +0900

    累計時間表示機能の実装

--- public/js/setTotalStudyTime.mjs
+++ public/js/setTotalStudyTime.mjs
@@ -1,16 +1,16 @@
-import { formatTime } from "./timerUtils.mjs";
+import { formatTimeMinutes } from "./timerUtils.mjs";
 import { fetchTotalStudyMinutes } from "./fetch_totalStudyMinutes.mjs";
 
 
-/* 
+/*
 
 合計勉強時間を設定する関数
 
 引数：秒 (DBの合計学習時間に可算する秒数)
 
-*/ 
+*/
 
-export async function setTotalStudyTime($second) { 
+export async function setTotalStudyTime($second) {
     // 累計合計時間を表示するHTMLタグのid名
     const totalTimerElement = document.getElementById("total-timer");
 
@@ -20,12 +20,7 @@ export async function setTotalStudyTime($second) {
         const totalStudyMinutes = totalStudyData[0].total_study_minutes;
 
         let totalSeconds = totalStudyMinutes ? totalStudyMinutes * 60 : 0; // 分を秒に変換
-
-        if ($second) { // 引数$secondが存在するなら加算
-            totalSeconds += $second;
-        }
-
-        const formattedTime = formatTime(totalSeconds); // 形式 HH:MM:SS
+        const formattedTime = formatTimeMinutes(totalSeconds); // 形式 HH:MM:SS
         totalTimerElement.innerText = formattedTime;
     } catch (error) {
         console.error("Error fetching total study minutes:", error);
--- public/js/timerUtils.mjs
+++ public/js/timerUtils.mjs
@@ -9,3 +9,16 @@ export function formatTime(time) {
     const secs = String(time % 60).padStart(2, '0');
     return `${hours}:${minutes}:${secs}`;
   }
+
+export function formatTimeMinutes(time) {
+    const hours = String(Math.floor(time / 3600)).padStart(2, '0');
+    const minutes = String(Math.floor((time % 3600) / 60)).padStart(2, '0');
+    return `${hours}h ${minutes}m`;
+}
+
+export function getTotalSeconds(studyHistoryList){
+    const totalTime = studyHistoryList.reduce((total, session) => {
+        return total + session.session_duration_minutes;
+    }, 0);
+    return totalTime;
+}
